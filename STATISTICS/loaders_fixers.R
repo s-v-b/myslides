3e7311244e2712c896e96b73c9185be358f82819
Stéphane Boucheron <boucheron@lpsm.paris>
2021-01-05 09:14:23 +0100

    cm-1 cours de Statistics WIP

--- STATISTICS/loaders_fixers.R
+++ STATISTICS/loaders_fixers.R
@@ -1,22 +1,24 @@
 
+require(plyr)
+require(tidyverse)
+require(readr)
 
-pacman::p_load(tidyverse)
-pacman::p_load(readr)
-pacman::p_load(latex2exp)
-pacman::p_load(kableExtra)
-pacman::p_load(gt)
+require(latex2exp)
+require(kableExtra)
+require(gt)
 # old_theme <-theme_set(theme_xaringan())
-old_theme <-theme_set(theme_bw(base_size=14, base_family = "Helvetica"))
+old_theme <-theme_set(theme_bw(base_size=14, 
+                               base_family = "Helvetica"))
 
-pacman::p_load("knitr")
+require("knitr")
 
-opts_chunk$set(warning = FALSE, 
-               message = FALSE, 
-               cache = TRUE, 
-               autodep = TRUE, 
-               tidy = FALSE, 
+opts_chunk$set(warning = FALSE,
+               message = FALSE,
+               cache = TRUE,
+               autodep = TRUE,
+               tidy = FALSE,
                fig.retina = 4)
 
 require(data.table, quietly = TRUE, warn.conflicts = FALSE)
 
-pacman::p_load(readr)
+require(readr)
