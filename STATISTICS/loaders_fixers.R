ad9538abaa638fd50d3dcbe3aa52904788868c07
Stéphane Boucheron <boucheron@lpsm.paris>
2021-01-06 22:47:05 +0100

    CM 1 Statistics almost OK

--- STATISTICS/loaders_fixers.R
+++ STATISTICS/loaders_fixers.R
@@ -6,8 +6,10 @@ require(readr)
 require(latex2exp)
 require(kableExtra)
 require(gt)
+require(latex2exp)
+
 # old_theme <-theme_set(theme_xaringan())
-old_theme <-theme_set(theme_bw(base_size=14, 
+old_theme <-theme_set(theme_bw(base_size=14,
                                base_family = "Helvetica"))
 
 require("knitr")
